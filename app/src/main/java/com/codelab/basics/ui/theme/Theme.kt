ea078a1c57f79ee2e0ee1c88835ce8582a1e3f48
Marcellus <marcellus13@hotmail.com>
2024-01-30 20:43:16 -0300

    card amarelo

--- app/src/main/java/com/codelab/basics/ui/theme/Theme.kt
+++ app/src/main/java/com/codelab/basics/ui/theme/Theme.kt
@@ -26,23 +26,22 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
-    surface = Blue,
-    onSurface = Navy,
-    primary = Navy,
+    surface = DarkYellow,
+    onSurface = DarkYellow,
+    primary = DarkYellow,
     onPrimary = Chartreuse
 )
 
 private val LightColorScheme = lightColorScheme(
-    surface = Blue,
-    onSurface = Color.White,
-    primary = LightBlue,
+    surface = Yellow,
+    onSurface = Yellow,
+    primary = Yellow,
     onPrimary = Navy
 )
 
